c805b893d2b40fbad0cfbdec25d5b9983773e575
EstebanCaponio <estebancaponio@gmail.com>
2025-03-07 14:54:54 +0100

    feat: created array of objects

--- esercizio-1/script.js
+++ esercizio-1/script.js
@@ -1,1 +1,21 @@
-console.log('esercizio 1');
+const bikes = [
+    {
+        'bikeName': 'bianchi',
+        'bikeWeight': 7
+    },
+
+    {
+        'bikeName': 'scott',
+        'bikeWeight': 6
+    },
+
+    {
+        'bikeName': 'genesis',
+        'bikeWeight': 12
+    },
+
+    {
+        'bikeName': 'focus',
+        'bikeWeight': 8
+    }
+]
