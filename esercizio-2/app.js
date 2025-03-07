d13515bfa0f944fb03a3b46fca2cd377b96c867c
EstebanCaponio <estebancaponio@gmail.com>
2025-03-07 16:49:29 +0100

    feat: created array with objects

--- esercizio-2/app.js
+++ esercizio-2/app.js
@@ -1,1 +1,38 @@
-console.log('esercizio 2');
+
+const teams = [
+    {
+        'teamName': 'italia',
+        'points': 0,
+        'foulsSuffered': 0
+    },
+
+    {
+        'teamName': 'spagna',
+        'points': 0,
+        'foulsSuffered': 0
+    },
+
+    {
+        'teamName': 'argentina',
+        'points': 0,
+        'foulsSuffered': 0
+    },
+
+    {
+        'teamName': 'francia',
+        'points': 0,
+        'foulsSuffered': 0
+    },
+
+    {
+        'teamName': 'germania',
+        'points': 0,
+        'foulsSuffered': 0
+    },
+
+    {
+        'teamName': 'inghilterra',
+        'points': 0,
+        'foulsSuffered': 0
+    }
+]
